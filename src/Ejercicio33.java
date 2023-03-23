df7d0313e89efa876882793982caf2cf2d6fb966
AlejandroLilloRodriguez <alejandro.lillorodriguez@usp.ceu.es>
2023-03-23 14:46:30 +0100

    Ejercicio 33 hecho

--- src/Ejercicio33.java
+++ src/Ejercicio33.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Ejercicio33 {
+public class Ejercicio33 {
+    public static int suma(int n){
+        if (n==1){
+            return 1;
+        }else{
+            return n+suma(n-1);
+        }
+    }
+    public static int suma1(int n){
+        int suma=0;
+        for (int i = 1; i <= n; i++) {
+            suma+=i;
+        }
+        return suma;
+    }
 }
